ffdba45f1e2bd4897a51ba372794b683ca30eddb
aurianer <aurianer@cscs.ch>
2020-06-11 18:09:50 +0200

    Change the print of the squeue

--- pycicle-watch.sh
+++ pycicle-watch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -i
 
-watch -n600 "squeue -u $USER -o '%.40j %.10S %.8M %.2t %.2D %10R %.8i'" --sort 'j'
+watch -n600 "squeue -u $USER -o '%40j %.10S %.8M %.2t %.2D %10R %.8i'" --sort 'j'
